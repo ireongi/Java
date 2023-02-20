0e51e1fa200b7f7677d745daf96c70b1fd4f18de
ireongi <ireongi@gmail.com>
2023-02-20 13:45:34 +0900

    Network

---
+++ University/BootCamp/src/Java_Network/D03/Day24.java
@@ -0,0 +1,22 @@
+package Java_Network.D03;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Day24 {
+
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress address = null;
+
+        InetAddress names[] = InetAddress.getAllByName("www.naver.com");
+        for(InetAddress element : names) {
+            System.out.println(element);
+        }
+
+        try {
+            address = InetAddress.getByName("www.packtpub.com");
+        } catch (UnknownHostException e){
+
+        }
+    }
+}
