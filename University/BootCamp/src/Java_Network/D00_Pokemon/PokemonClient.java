820d8e89b93d5e8d626e870d9e730b99c0be5bea
ireongi <ireongi@gmail.com>
2023-02-21 14:03:19 +0900

    Network Final

---
+++ University/BootCamp/src/Java_Network/D00_Pokemon/PokemonClient.java
@@ -0,0 +1,36 @@
+package Java_Network.D00_Pokemon;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+public class PokemonClient {
+    public PokemonClient() {
+        SocketAddress address = new InetSocketAddress("127.0.0.1", 3000);
+        try (SocketChannel socketChannel = SocketChannel.open(address)) {
+            System.out.println("포켓몬 서버 접속됨");
+            String message;
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.println("서버 메세지 수신 대기중...");
+                System.out.println("서버 메세지: ");
+                System.out.println(HelperMethods.receiveMessage(socketChannel));
+                System.out.print("> ");
+                message = scanner.nextLine();
+                if (message.equals("q")) {
+                    HelperMethods.sendMessage(socketChannel, message);
+                    break;
+                }
+                HelperMethods.sendMessage(socketChannel, message);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new PokemonClient();
+    }
+}
