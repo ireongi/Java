6c13429af776cfabb52f2269d1f93dcf98ece049
ireongi <ireongi@gmail.com>
2023-02-26 00:05:00 +0900

    GUI

---
+++ Basic/src/GUI/_1_HelloSwing.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.swing.*;
+
+public class _1_HelloSwing {
+    public static void main(String[] args) {
+        JFrame helloSwing = new JFrame(); // 프레임 생성
+
+        helloSwing.setTitle("Hello Swing"); // 프레임 제목 설정
+        helloSwing.setSize(200, 100); // 프레임 크기 설정
+
+        helloSwing.setVisible(true); // 화면에 표시
+    }
+}
